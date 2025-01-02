6864a682ad4ee600bf248ce8f2d39cde7e63b5bf
s-uxun <serenasol@ewha.ac.kr>
2025-01-02 17:08:00 +0900

    Feat: Add Logo component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Header } from "src/common/Header";
 import { Sidebar } from "src/common/Sidebar";
+import { Logo } from "src/assets/icons";
 
 export default function Home() {
   return (
     <>
+      <Logo />
       <Header />
       <Sidebar />
     </>
--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,2 +1,3 @@
 export { default as Close } from "./Close";
 export { default as Hamburger } from "./Hamburger";
+export { default as Logo } from "./Logo";
