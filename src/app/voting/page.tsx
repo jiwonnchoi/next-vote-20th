3b0ec308f231f7f11f283feb4156052269ec0186
s-uxun <serenasol@ewha.ac.kr>
2025-01-03 11:35:58 +0900

    Feat: Create basic structure & design for voting page

--- src/app/voting/page.tsx
+++ src/app/voting/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { LeaderBf } from "src/assets/icons";
 import { DemoBf } from "src/assets/icons";
 
@@ -7,8 +8,23 @@ export default function Home() {
       className="bg-gradient-to-b from-white via-[#D2E6FD] to-white w-full mt-24 flex flex-col pt-16"
       style={{ height: "35.5rem" }}
     >
-      <LeaderBf className="w-[17.5rem] ml-[8%] mb-[2.62rem]" />
-      <DemoBf className="self-end w-[12.6rem] mr-[10%]" />
+      <Link href="/voting/leader" className="ml-[8%] mb-10">
+        <div className="relative w-[17.5rem]">
+          {" "}
+          <LeaderBf />
+          <span className="Body_2_med absolute top-[4.31rem] left-[4.63rem]">
+            FE - LEADER
+          </span>
+        </div>
+      </Link>
+      <Link href="/voting/demoday" className="self-end mr-[10%]">
+        <div className="relative w-[12.6rem]">
+          <DemoBf />
+          <span className="Body_2_med absolute top-[7.1rem] right-[4.56rem]">
+            DEMO - DAY
+          </span>
+        </div>
+      </Link>
     </div>
   );
 }
