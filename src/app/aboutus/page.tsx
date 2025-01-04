1036abed7a8b706aabcc4b09b1eeaab9fed514b0
s-uxun <serenasol@ewha.ac.kr>
2025-01-04 22:26:47 +0900

    Design: Add pc design into about us page

--- src/app/aboutus/page.tsx
+++ src/app/aboutus/page.tsx
@@ -1,14 +1,30 @@
+import Image from "next/image";
+import AboutUsPc from "src/assets/images/AboutUs_pc.png";
 import { AboutUsMobile } from "src/assets/icons";
+
 export default function Aboutus() {
   return (
-    <div className="flex flex-col w-full relative">
-      <div className="z-0 absolute w-[7.5625rem] h-[2.6875rem] bg-Blue_Grey top-[7.94rem] ml-5"></div>
-      <div className="z-2 relative Headline_1 mt-[7.94rem] ml-[1.63rem]">
-        MADE BY.
+    <>
+      <div className="flex flex-col w-full relative pc:hidden">
+        <div className="z-0 absolute w-[7.5625rem] h-[2.6875rem] bg-Blue_Grey top-[7.94rem] ml-5"></div>
+        <div className="z-2 relative Headline_1 mt-[7.94rem] ml-[1.63rem]">
+          MADE BY.
+        </div>
+        <div className="self-center w-[80%] mb-24">
+          <AboutUsMobile className="w-full mt-[2.69rem]" />
+        </div>
       </div>
-      <div className="self-center w-[80%] mb-24">
-        <AboutUsMobile className="w-full mt-[2.69rem]" />
+      <div
+        className="relative w-[60%] hidden mt-36 pc:flex"
+        style={{ aspectRatio: "56 / 35" }}
+      >
+        <Image
+          src={AboutUsPc}
+          alt="About Us"
+          layout="fill"
+          objectFit="contain"
+        />
       </div>
-    </div>
+    </>
   );
 }
